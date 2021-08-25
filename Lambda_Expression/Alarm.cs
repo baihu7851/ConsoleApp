d63ee559d3985b54c755f400afd917a0f678b99d
ITSOWER\Baihu <baihu7851@gmail.com>
2021-08-25 18:00:47 +0800

    增加summary、增加判斷

--- Lambda_Expression/Alarm.cs
+++ Lambda_Expression/Alarm.cs
@@ -2,27 +2,45 @@
 
 namespace Lambda_Expression
 {
+    /// <summary>
+    /// 溫度警報器
+    /// </summary>
     class Alarm
     {
+        /// <summary>
+        /// 警報器初始化
+        /// </summary>
+        /// <param name="alarmTemperature">報警溫度</param>
+        /// <param name="fireHardHandler">報警動作</param>
         public Alarm(int alarmTemperature,Action<int> fireHardHandler)
         {
             this.AlarmTemperature = alarmTemperature;
             this.CurrentTemperature = alarmTemperature;
-            FireHardHandler = fireHardHandler;
+            this.FireHardHandler = fireHardHandler;
         }
         int CurrentTemperature;
         int AlarmTemperature;
         Action<int> FireHardHandler;
-
+        /// <summary>
+        /// 是否超過溫度
+        /// </summary>
         public bool IsOverheat { get { return CurrentTemperature >= AlarmTemperature; } }
+        /// <summary>
+        /// 變更報警動作
+        /// </summary>
+        /// <param name="fireHardHandler">報警動作</param>
         public void ChangeFireHardHandler(Action<int> fireHardHandler)
         {
             FireHardHandler = fireHardHandler;
             Console.WriteLine("ChangeOK");
         }
+        /// <summary>
+        /// 溫度偵測
+        /// </summary>
+        /// <param name="temperature">目前溫度</param>
         public void ChangeTemperature(int temperature)
         {
-            this.CurrentTemperature = temperature;
+            CurrentTemperature = temperature;
             if (IsOverheat && FireHardHandler!=null)
             {
                 FireHardHandler(temperature);
--- Lambda_Expression/Program.cs
+++ Lambda_Expression/Program.cs
@@ -7,15 +7,17 @@ namespace Lambda_Expression
         static void Main(string[] args)
         {
             Alarm alarm = new Alarm(70,x=>Console.WriteLine($"現在溫度為{x}度，已通知消防隊"));
-            //alarm.ChangeTemperature(69);
-            //alarm.ChangeTemperature(70);
-            //alarm.ChangeTemperature(71);
-            //alarm.ChangeTemperature(15);
-            //alarm.ChangeTemperature(105);
-            //alarm.ChangeFireHardHandler(x => Console.WriteLine("失火啦！"));
-            //alarm.ChangeTemperature(99);
-            //alarm.ChangeTemperature(15);
-            //alarm.ChangeTemperature(105);
+            alarm.ChangeTemperature(69);
+            alarm.ChangeTemperature(70);
+            alarm.ChangeTemperature(71);
+            alarm.ChangeTemperature(15);
+            alarm.ChangeTemperature(105);
+            alarm.ChangeFireHardHandler(x => Console.WriteLine("失火啦！"));
+            alarm.ChangeTemperature(69);
+            alarm.ChangeTemperature(70);
+            alarm.ChangeTemperature(71);
+            alarm.ChangeTemperature(15);
+            alarm.ChangeTemperature(105);
             Console.ReadKey();
         }
     }
